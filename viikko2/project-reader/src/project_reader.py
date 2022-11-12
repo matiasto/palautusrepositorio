a5a17908bb25993e064f9b90a867b2ef24448a63
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-12 23:08:16 +0200

    feat(viikko2): tehtävä 3

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,19 @@
+from urllib import request
+from toml import loads
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        parsed_content = loads(content)
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(parsed_content["tool"]["poetry"]["name"], 
+                        parsed_content["tool"]["poetry"]["description"], 
+                        list(parsed_content["tool"]["poetry"]["dependencies"].keys()), 
+                        list(parsed_content["tool"]["poetry"]["dev-dependencies"].keys()))
