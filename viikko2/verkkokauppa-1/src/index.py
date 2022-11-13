735b6c2584db75b2fa63a91b79e12b117e10a36e
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-14 01:55:50 +0200

    feat(viikko2): tehtava 12

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,16 +1,9 @@
 from kauppa import Kauppa
-from kirjanpito import Kirjanpito
-from pankki import Pankki
-from varasto import Varasto
-from viitegeneraattori import Viitegeneraattori
 
 
 def main():
-    viitegeneraattori = Viitegeneraattori()
-    kirjanpito = Kirjanpito()
-    varasto = Varasto(kirjanpito)
-    pankki = Pankki(kirjanpito)
-    kauppa = Kauppa(varasto, pankki, viitegeneraattori)
+
+    kauppa = Kauppa()
 
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
@@ -27,10 +20,6 @@ def main():
         kauppa.lisaa_koriin(5)
 
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
-
-    # kirjanpito
-    for tapahtuma in kirjanpito.tapahtumat:
-        print(tapahtuma)
 
 
 if __name__ == "__main__":
