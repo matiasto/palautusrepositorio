5c972a880f93e98040fbf35baf3bcb3479f2f8bf
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-26 22:25:34 +0200

    feat(viikko4): 5.5

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         juusto = Tuote("Juusto", 5)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(juusto)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta(self):
+        maito = Tuote("Maito", 3)
+        juusto = Tuote("Juusto", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(juusto)
+        self.assertEqual(self.kori.hinta(), 8)
