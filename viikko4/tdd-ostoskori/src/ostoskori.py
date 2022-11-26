f53f72151b35493d6e7587feef81275f76ff5fa0
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-26 22:29:06 +0200

    feat(viikko4): 5.8

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,24 +4,24 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.ostokset = {}
+        self._ostokset = {}
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
-        return sum([ostos.lukumaara() for ostos in self.ostokset.values()])
+        return sum([ostos.lukumaara() for ostos in self._ostokset.values()])
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        return sum([ostos.hinta() for ostos in self.ostokset.values()])
+        return sum([ostos.hinta() for ostos in self._ostokset.values()])
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        if lisattava.nimi() in self.ostokset:
-            self.ostokset[lisattava.nimi()].muuta_lukumaaraa(1)
+        if lisattava.nimi() in self._ostokset:
+            self._ostokset[lisattava.nimi()].muuta_lukumaaraa(1)
         else:
-            self.ostokset[lisattava.nimi()] = Ostos(lisattava)
+            self._ostokset[lisattava.nimi()] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -32,6 +32,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return list(self._ostokset.values())
